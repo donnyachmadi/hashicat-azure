c1504c5cc4f1f0def1247cd1ddb6cddddf3a16da
Donny Achmadi <donny216@gmail.com>
2022-03-16 03:02:34 +0000

    added billable tag

--- main.tf
+++ main.tf
@@ -137,7 +137,7 @@ resource "azurerm_virtual_machine" "catapp" {
 
   tags = {
     Department = "devops"
-    Apps = "Web"
+    Billable = "true"
   }
 
   # Added to allow destroy to work correctly.
